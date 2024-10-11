fbeaac1b60a164c60c610278daf5c9781436f271
subas-roy <subasroy46@gmail.com>
2024-10-12 05:43:50 +0600

    Create client Side router

---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,10 @@
+
+const AddCoffee = () => {
+  return (
+    <div>
+      <h2>Add coffee</h2>
+    </div>
+  );
+};
+
+export default AddCoffee;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import AddCoffee from './components/AddCoffee.jsx';
+import UpdateCoffee from './components/UpdateCoffee.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "/addCoffee",
+    element: <AddCoffee/>
+  },
+  {
+    path: "/updateCoffee",
+    element: <UpdateCoffee/>
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
