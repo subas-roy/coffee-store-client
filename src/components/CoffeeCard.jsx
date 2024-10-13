8f6272b2a336527c3e58de46d6ce031a49bddf7c
subas-roy <subasroy46@gmail.com>
2024-10-13 20:38:11 +0600

    Delete a coffee and fetch coffee for update

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,6 +1,39 @@
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const CoffeeCard = ({ coffee }) => {
-  const { name, quantity, supplier, taste, category, details, photo } = coffee;
+  const { _id, name, quantity, supplier, taste, category, details, photo } = coffee;
+  const handleDelete = _id => {
+    console.log(_id)
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        console.log('delete confirmed!')
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+          method: 'DELETE'
+        })
+          .then(res => res.json())
+          .then(data => {
+            console.log(data)
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+              });
+            }
+          })
+      }
+    });
+  }
+
   return (
     <div className="card card-side bg-base-100 shadow-xl">
       <figure>
@@ -17,10 +50,12 @@ const CoffeeCard = ({ coffee }) => {
         </div>
         <div className="card-actions justify-end">
           <div className="join join-vertical space-y-4">
-          <button className="btn join-item">View</button>
-          <button className="btn join-item">Edit</button>
-          <button className="btn join-item">X</button>
-        </div>
+            <Link to={`/updataCoffee/${_id}`}>
+              <button className="btn join-item">View</button>
+            </Link>
+            <button className="btn join-item">Edit</button>
+            <button onClick={() => handleDelete(_id)} className="btn join-item bg-red-500">X</button>
+          </div>
         </div>
       </div>
     </div>
